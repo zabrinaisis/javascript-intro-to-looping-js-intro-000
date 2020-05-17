82e40dcef140fefd8bde99d88cc29e68fa78988e
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-17 17:30:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
     if (i === 1) {
       array.push("I am 1 strange loop.");
     } else {
-      array.push(`I am ${i} strange loops.`)
+      array.push(`I am ${i} strange loops.`);
     }
   }
   return (array)
@@ -13,4 +13,5 @@ function whileLoop(num) {
   while (num > 0) {
     console.log(--num);
   }
+  return ("done");
 }
