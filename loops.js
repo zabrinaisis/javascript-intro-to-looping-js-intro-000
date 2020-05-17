41708de8f5dcde2e007a8d8787142e4af3aded1b
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-17 17:25:31 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,10 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-    
+      array.pop("I am 1 strange loop.");
+    } else {
+      array.pop(`I am ${i} strange loops.`)
     }
   }
+  return (array)
 }
