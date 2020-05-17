4ba413588f65d5eddc93ced55811189adf7c4055
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-17 17:39:52 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -26,5 +26,5 @@ function incrementVariable() {
 function doWhileLoop(num) {
   do {
     console.log("I run once regardless.");
-  } while (incrementVariable() < num);
+  } while (incrementVariable() < num+1);
 }
